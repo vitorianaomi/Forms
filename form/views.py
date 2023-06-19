fcae3782e136fbd328f4c4a7dcd2d54f165847d4
VITÓRIA NAOMÍ MARTINS REIS <vitoria.naomi@academico.ifrn.edu.br>
2023-06-19 08:27:27 -0300

    third commit

--- form/views.py
+++ form/views.py
@@ -5,15 +5,15 @@ from .models import Aluno
 # Create your views here.
 def cadastro_aluno(request): #renderizar e cadastrar
     if request.method == 'POST':  
-        form = AlunoForm(request.POST) 
-        if form.is_valid():
-            nome = form.cleaned_data['nome']
+        form = AlunoForm(request.POST) #pega tudo da requisição e coloca no form
+        if form.is_valid(): #os valores são válidos? True ou False
+            nome = form.cleaned_data['nome'] #form.cleaned_data[]: dicionário com os tipos convertidos e corretos
             email = form.cleaned_data['email']
             data_nascimento = form.cleaned_data['data_nascimento']
             #salvando o aluno no banco.
             #Aluno.objects.create(nome=nome, email=email,data_nascimento=data_nascimento)
             print("Nome: {} - Email: {} - Data de Nascimento: {}".format(nome,email,data_nascimento))
-    else:
+    else: #primeira vez sempre vai ser get; só entra na parte acima quando for dado o primeiro submit
         print("->>>> entrou primeiro aqui")
         form = AlunoForm() #instanciando a classe do formulário
 

25811c290a218eda0d5af2808d5f91e9b495a471
VITÓRIA NAOMÍ MARTINS REIS <vitoria.naomi@academico.ifrn.edu.br>
2023-06-19 08:20:37 -0300

    second commit

--- estudo_formulario/urls.py
+++ estudo_formulario/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from form.views import cadastro_aluno
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cadastro_aluno/', cadastro_aluno, name='cadastro_aluno'),
 ]
--- form/views.py
+++ form/views.py
@@ -3,9 +3,9 @@ from .forms import AlunoForm
 from .models import Aluno
 
 # Create your views here.
-def cadastro_aluno(request): #renderizar o formulário(criar); quando o usuário clica e coloca as informações
+def cadastro_aluno(request): #renderizar e cadastrar
     if request.method == 'POST':  
-        form = AlunoForm(request.POST)
+        form = AlunoForm(request.POST) 
         if form.is_valid():
             nome = form.cleaned_data['nome']
             email = form.cleaned_data['email']
@@ -15,6 +15,6 @@ def cadastro_aluno(request): #renderizar o formulário(criar); quando o usuário
             print("Nome: {} - Email: {} - Data de Nascimento: {}".format(nome,email,data_nascimento))
     else:
         print("->>>> entrou primeiro aqui")
-        form = AlunoForm()
+        form = AlunoForm() #instanciando a classe do formulário
 
-    return render(request, 'escola/form_aluno.html', {'form': form})
+    return render(request, 'form/form_aluno.html', {'form': form}) #colocando em um contexto e enviando form para página
